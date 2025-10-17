056573d11b387e55bdc2906fc13423013a8ed2b2
Ti0Elvis <elvis.veramijares@gmail.com>
2025-10-17 09:23:23 +0200

    Refactor init_stack.c: Update author information and improve formatting of append_node function

--- src/init_stack.c
+++ src/init_stack.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   init_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evera <evera@student.42.fr>                +#+  +:+       +#+        */
+/*   By: e06 <e06@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:15:33 by evera             #+#    #+#             */
-/*   Updated: 2025/10/09 16:15:34 by evera            ###   ########.fr       */
+/*   Updated: 2025/10/17 09:23:12 by e06              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*append_node(t_stack *stack, long value)
+t_stack *append_node(t_stack *stack, long value)
 {
-	t_stack	*temp;
-	t_stack	*new_node;
+	t_stack *temp;
+	t_stack *new_node;
 
 	if (value < INT_MIN || value > INT_MAX)
-		return (NULL);
+		return (free_stack(stack), NULL);
 	new_node = (t_stack *)malloc(sizeof(t_stack));
 	if (!new_node)
-		return (NULL);
+		return (free_stack(stack), NULL);
 	new_node->value = (int)value;
 	new_node->next = NULL;
 	new_node->index = 0;
@@ -34,12 +34,12 @@ t_stack	*append_node(t_stack *stack, long value)
 	return (stack);
 }
 
-t_stack	*init_stack(char **numbers)
+t_stack *init_stack(char **numbers)
 {
-	int		i;
-	int		j;
-	char	**split;
-	t_stack	*stack;
+	int i;
+	int j;
+	char **split;
+	t_stack *stack;
 
 	i = 0;
 	stack = NULL;
